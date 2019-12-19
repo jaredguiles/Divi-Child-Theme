75e26e1399780f92fa8fd95e00e71cc51e9fa103
Jared Guiles <jared.guiles@booking.com>
2019-12-19 15:45:30 -0800

    Update

--- 404.php
+++
@@ -1,3 +0,0 @@
-<?php get_header(); ?>
-<?php echo do_shortcode('[et_pb_section global_module="138109"][/et_pb_section]'); ?>
-<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,31 +1,18 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
-require 'theme-update-checker/theme-update-checker.php';
-$myUpdateChecker = new Puc_v4p7_Vcs_themeUpdateChecker(
-    new Puc_v4p7_Vcs_GitLabApi('https:/https://gitlab.com/jguiles-wordpress-themes/divi-child-theme'),
-    __FILE__,
-    'divi-cild-theme'
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+include 'theme-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://gitlab.com/jguiles-wordpress-themes/divi-child-theme',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'divi-child-theme'
 );
 $myUpdateChecker->setAuthentication('JZrxETpiuYtQKHqN8LVf');
 
-// BEGIN ENQUEUE PARENT ACTION
-
-if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
-    function chld_thm_cfg_locale_css( $uri ){
-        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
-            $uri = get_template_directory_uri() . '/rtl.css';
-        return $uri;
-    }
-endif;
-add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
-
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
-    }
-endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
-
-// END ENQUEUE PARENT ACTION
+// BEGIN ENQUE CUSTOM LOGIN //
+include( plugin_dir_path( __FILE__ ) . 'login/functions.php' );
+// END ENQUE CUSTOM LOGIN //
